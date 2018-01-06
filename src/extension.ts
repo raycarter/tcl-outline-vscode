cd79ad84bdb3b11503d768eb6012da5e4aa6ed5b
Li Sheng <li-sheng@outlook.com>
2018-01-06 14:05:44 +0100

    use only 2 spaces for indent

--- src/extension.ts
+++ src/extension.ts
@@ -3,22 +3,22 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    // output
-    let channel = vscode.window.createOutputChannel('tcl outline');
-    let tclOutline = new TclOutline(channel);
-    let tclOutlineUnsorted = vscode.commands.registerCommand('extension.tclOutlineUnsorted', () => {
-        
-        tclOutline.parseCurrentFile(false);
-        
-    });
-    context.subscriptions.push(tclOutlineUnsorted);
+  // output
+  let channel = vscode.window.createOutputChannel('tcl outline');
+  let tclOutline = new TclOutline(channel);
+  let tclOutlineUnsorted = vscode.commands.registerCommand('extension.tclOutlineUnsorted', () => {
+    
+    tclOutline.parseCurrentFile(false);
+    
+  });
+  context.subscriptions.push(tclOutlineUnsorted);
 
-    let tclOutlineSorted = vscode.commands.registerCommand('extension.tclOutlineSorted', () => {
-        
-        tclOutline.parseCurrentFile(true);
-        
-    });
-    context.subscriptions.push(tclOutlineSorted);
+  let tclOutlineSorted = vscode.commands.registerCommand('extension.tclOutlineSorted', () => {
+    
+    tclOutline.parseCurrentFile(true);
+    
+  });
+  context.subscriptions.push(tclOutlineSorted);
 
 }
 
@@ -27,55 +27,55 @@ export function deactivate() {
 }
 
 class TclOutline {
-    private channel: vscode.OutputChannel;
-    constructor (ch: vscode.OutputChannel) {
-        this.channel = ch;
+  private channel: vscode.OutputChannel;
+  constructor (ch: vscode.OutputChannel) {
+    this.channel = ch;
+  }
+  parseCurrentFile (sorted: boolean) {
+    let text = vscode.window.activeTextEditor.document.getText();
+
+    
+    let procReg = new RegExp('^\s*proc (.*)');
+    let procFoundList = [];
+    // parse text for proc
+    let foundCount = 0, lineNum = 0;
+    for (let line of text.split('\n')) {
+      lineNum++;
+      let procFound = procReg.exec(line);
+      if (procFound !== null) {
+        foundCount++;
+        let u = vscode.window.activeTextEditor.document.uri.path;
+        let anchor = `${vscode.window.activeTextEditor.document.uri}#${lineNum}`;
+        // channel.appendLine(anchor);
+        let foundStr = procFound[1];
+        if (foundStr !== '' && foundStr.substr(foundStr.length-1, 1) === '{') {
+          foundStr = foundStr.slice(0, -1);
+        }
+        procFoundList.push(foundStr + ' -- ' + anchor + '\n');
+      }
     }
-    parseCurrentFile (sorted: boolean) {
-        let text = vscode.window.activeTextEditor.document.getText();
 
-        
-        let procReg = new RegExp('^\s*proc (.*)');
-        let procFoundList = [];
-        // parse text for proc
-        let foundCount = 0, lineNum = 0;
-        for (let line of text.split('\n')) {
-            lineNum++;
-            let procFound = procReg.exec(line);
-            if (procFound !== null) {
-                foundCount++;
-                let u = vscode.window.activeTextEditor.document.uri.path;
-                let anchor = `${vscode.window.activeTextEditor.document.uri}#${lineNum}`;
-                // channel.appendLine(anchor);
-                let foundStr = procFound[1];
-                if (foundStr !== '' && foundStr.substr(foundStr.length-1, 1) === '{') {
-                    foundStr = foundStr.slice(0, -1);
-                }
-                procFoundList.push(foundStr + ' -- ' + anchor + '\n');
-            }
-        }
-
-        if (sorted) {
-            procFoundList.sort();
-        }
-
-        // write to output
-        this.channel.clear();
-        this.channel.show(true);
-        for (let resLine of procFoundList) {
-            this.channel.appendLine(`${resLine}`);
-        }
+    if (sorted) {
+      procFoundList.sort();
     }
+
+    // write to output
+    this.channel.clear();
+    this.channel.show(true);
+    for (let resLine of procFoundList) {
+      this.channel.appendLine(`${resLine}`);
+    }
+  }
 
 
 }
 
 class TclOutlineInfo {
-    private fileName: string;
-    private lineNum: number;
+  private fileName: string;
+  private lineNum: number;
 
-    constructor (fn: string, num: number) {
-        this.fileName = fn;
-        this.lineNum = num;
-    }
+  constructor (fn: string, num: number) {
+    this.fileName = fn;
+    this.lineNum = num;
+  }
 }
