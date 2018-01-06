7b0fa7b12f7ce72cab1a6ac80d9cad9c5f1d9d4c
Li Sheng <li-sheng@outlook.com>
2018-01-06 14:51:21 +0100

    add some line of tcl code in test file

--- test/test.tcl
+++ test/test.tcl
@@ -1,45 +1,67 @@
 
-
-proc abc {a b} {
-
+#****f* test/test
+# NAME
+#  useMsgBox
+# DESCRIPTION
+#  nothing important
+# AUTHOR
+#  SL
+# CREATION DATE 
+#  11.12.2016
+proc useMsgBox {msg} {
+  global globStrings
+  tk_messageBox -icon error -title [msg_get "Server:"]  -message "$msg"
 }
 
-proc def {d e} {
-
+#****f* test/test
+# NAME
+#  sendErrorToServer
+# DESCRIPTION
+#  nothing important
+# AUTHOR
+#  SL
+# CREATION DATE 
+#  11.12.2016
+proc sendErrorToServer {msg error} {
+  global errorInfo
+  set clockseconds [clock seconds]
+  set txt [msg_get "Error "]
+  append txt $clockseconds
+  set errordisplay "$txt\n $msg"
+  set errorlog "$msg - $error - $errorInfo"
+  set errorextendedlog "CLIENT VARIABLES [errorvariables]"
+  client_send [list write2errorlog $clockseconds $errorlog $errorextendedlog errorwriterresult]
 }
 
-proc aaa {a b c} {
-	
+#****f* test/test
+# NAME
+#  hideSomething
+# DESCRIPTION
+#  nothing important
+# AUTHOR
+#  SL
+# CREATION DATE 
+#  11.12.2016
+proc hideSomething {} {
+  global globVars globConst
+  if {[winfo exists $globConst(workframe)]} {
+    pack forget $globConst(workframe)
+    pack forget $globConst(toolframe)
+    pack forget $globConst(sepframe)
+    MenuDisable $globConst(menuframe)
+  }
 }
 
-proc fff eee {
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#****f* test/test
+# NAME
+#  onlyOne
+# DESCRIPTION
+#  nothing important
+# AUTHOR
+#  SL
+# CREATION DATE 
+#  11.12.2016
+proc onlyOne eee {}
 
 
 
